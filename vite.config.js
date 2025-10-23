fb7e71a698844fee22af23b1a24f79a6d0b84ea3
Ajmal Rishad <ajmalrishad@Ajmals-MacBook-Air.local>
2025-10-23 21:24:27 +0530

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/GI-TECH/', // add this line
   plugins: [react()],
-  base: '/', // ✅ Fix for Netlify
-});
+})
